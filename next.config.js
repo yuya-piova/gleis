a2f3668bddbffb1f2d849acdc5cfac2830b08299
yuya-piova <piova.sr.yt1211@gmail.com>
2025-12-13 20:37:05 +0900

    feat: Final PWA implementation and layout update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public', // サービスワーカーの出力先
+  disable: process.env.NODE_ENV === 'development', // 開発環境では無効にする
+  // PWAに必要なファイルをVercelでホストできるように設定
+  publicExcludes: [
+    '!manifest.webmanifest',
+    '!icons/icon-*.png',
+    '!robots.txt',
+    '!sitemap.xml',
+  ],
+});
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
